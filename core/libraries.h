d29e41471e1d88b28bda28a4f972597851c5f681
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2023-11-23 02:03:01 +0500

    Update and rename Linkers.asm to libraries.h

---
+++ core/libraries.h
@@ -0,0 +1,35 @@
+
+
+
+.def SUB16 =        $F016
+.def MUL16 =        $F01D
+.def UDIV16 =       $F074
+.def SDIV16 =       $F037
+.def CMP16 =        $F0B5
+.def ABS =          $F0D9
+.def DOPHL =        $F0E3
+.def DOPBC =        $F0EB
+.def DOPDE =        $F0F3
+;
+.def BITSET =       $F0FB
+.def BITCLR =       $F10F
+.def BITTST =       $F123
+.def MFILL =        $F12F
+.def COPCOUNT =     $F139
+.def SYM_IS_DEC =   $F148
+.def SYM_IS_HEX =   $F14F
+.def BCDASC =       $F167
+.def ASCBCD =       $F15A
+.def BCDSEG7 =      $F17A
+.def ASCSEG7 =      $F171
+.def BN2HEX =       $F193
+.def HEX2BN =       $F1B0
+.def LOWER =        $F1C7
+.def HIGHER =       $F1D0
+.def STRCOP =       $F1D9
+.def STRCMP =       $F1E5
+.def CONCAT =       $F209
+.def POS =          $F262
+;
+.def Function =     $F2B0
+.def Delay_ms_6 =   $F2BF
